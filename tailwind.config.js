aa884eda10c8dd821616b6c0678c8eaf06c5e729
jtcheng26 <jtcheng226@gmail.com>
2021-07-16 16:04:40 -0400

    Add assets and tailwind colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        brand: colors.violet,
+        brand: {
+          default: "#EAA53A",
+        },
+        gray: {
+          darkest: "#0A0F1B",
+          darker: "#0F172A",
+          dark: "#1E293B",
+          default: "#475569",
+          light: "#94A3B8",
+          lighter: "#E2E8F0",
+          lightest: "#F8FAFC",
+        },
       },
       fontFamily: {
         sans: ["Inter", "system ui", "Helvetica", "Arial", "sans-serif"],
